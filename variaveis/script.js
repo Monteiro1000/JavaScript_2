f979ffd61b6843e33e5c94d6b2a89cc4e5b7ecb4
Júlio Monteiro <julioalvedmonteiro@gmail.com>
2026-01-25 23:32:21 -0300

    começando novo curso de JavaScript. Dessa vez, na origamid.

---
+++ variaveis/script.js
@@ -0,0 +1,21 @@
+// var nome = 'André';
+// let idade = 28;
+// const possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+//variaveis
+
+//evitam repetições
+
+var preco = 25;
+var totalComprado = 5;
+var precoTotal = preco * totalComprado;
+
+console.log(precoTotal);
+
+var nome = 'André',
+    idade = 28,
+    possuiFaculdade = true;
+
+    console.log(nome, idade, possuiFaculdade);
+
