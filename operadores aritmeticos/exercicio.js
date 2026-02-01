1dde32e27ce34a06a28f5d3eb109a7d8ca6dbc73
Júlio Monteiro <julioalvedmonteiro@gmail.com>
2026-02-01 14:37:43 -0300

    Aprendendo sobre operadores aritméticos

---
+++ operadores aritmeticos/exercicio.js
@@ -0,0 +1,36 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20; //35
+console.log(total);
+
+// Crie duas expressões que retornem NaN
+var pesoPorDois = '80kg' / 2;
+console.log(pesoPorDois);
+
+var altura = '175cm';
+var peso2 = 71;
+var divisao = altura / peso2;
+console.log(divisao);
+
+var teste = 'teste' / 2;
+console.log(teste);
+
+
+// Somar a string '200' com o número 50 e retornar 250
+
+let soma = +'200' + 50;
+console.log(soma)
+
+
+// Incremente o número 5 e retorne o seu valor incrementado
+
+var cinco = 5;
+console.log(cinco++)
+console.log(cinco);
+
+// Como dividir o peso por 2?
+var numero = +'80'/ 2;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+
+
+console.log(peso);
---
+++ operadores aritmeticos/script.js
@@ -0,0 +1,27 @@
+// var idade = 2e10;
+
+// console.log(idade);
+
+// var soma = 100 + 50; // 150
+// var subtracao = 100 - 50; // 50
+// var multiplicacao = 100 * 2; // 200
+// var divisao = 100 / 2; // 50
+// var expoente = 2 ** 4; // 16
+// var modulo = 14 % 5; // 4
+
+// console.log(soma, subtracao, multiplicacao, divisao, expoente, modulo);
+
+// var testeNaN = 'Isso é 100' / 2;
+// console.log(isNaN(testeNaN));
+
+// var numero = 80;
+// var unidade = 'kg'
+// var peso = numero + unidade;
+// var pesoPorDois = peso / 2;
+// console.log(isNaN(pesoPorDois));
+
+// var soma1 = 10 + 10 + 20 + 30 * 4 / 2 + 10;
+// console.log(soma1);
+
+
+
