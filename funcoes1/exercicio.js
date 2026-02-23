4548863be46699588cc24be4c85e801795a6c277
Júlio Monteiro <julioalvedmonteiro@gmail.com>
2026-02-22 21:07:47 -0300

    Terminando parte de function

--- funcoes1/exercicio.js
+++ funcoes1/exercicio.js
@@ -1,69 +1,68 @@
 // Crie uma função para verificar se um valor é Truthy
 
-var masculino = true;
-var feminino = false;
-
-function teste (masculino) {
-
-    if (' ') {
-        
-        console.log (' É do sexo masculino');
-    } else {
-    console.log ('É do sexo feminino');
+function isTruthy(dado) {
+    return !!dado;
 }
 
-}
-console.log(teste(masculino));
+console.log(isTruthy('Oi'))
+
 
 // Crie uma função matemática que retorne o perímetro de um quadrado
-function ladoQuadrado () {
-    return 10;
+function perimetroQuadrado (lado) {
+    return lado * 4;
 }
 
-var perimetro = ladoQuadrado() * 4;
-console.log(perimetro);
+
 // lembrando: perímetro é a soma dos quatro lados do quadrado
 
 // Crie uma função que retorne o seu nome completo
-function nomeCompleto() {
-    var nome = 'Júlio';
-    var sobrenome = 'Monteiro'
-    return `${nome} ${sobrenome}`;
+function nomeCompleto(nome, sobrenome) {
+    return `${nome} ${sobrenome}`
 }
-console.log(nomeCompleto())
+console.log(nomeCompleto('Júlio', 'Monteiro'))
 
 // ela deve possuir os parâmetros: nome e sobrenome
 
 // Crie uma função que verifica se um número é par
 
-function numimpar () {
-    var num1 = 4
-
-    if (num1 % 2 == 0 ) {
-        console.log('PAR');
-    }else{
-        console.log('ÍMPAR');
+function isEven(numero) {
+    var modulo = numero % 2;
+    if(modulo === 0) {
+        return true;
+    } else {
+        return false;
     }
-    
 }
-
-console.log(numimpar())
 
 // Crie uma função que retorne o tipo de
 // dado do argumento passado nela (typeof)
+
+function tipoDeDado(dado) {
+    return typeof dado;
+}
+
+console.log(tipoDeDado('nome'))
 
 // addEventListener é uma função nativa do JavaScript
 // o primeiro parâmetro é o evento que ocorre e o segundo o Callback
 // utilize essa função para mostrar no console o seu nome completo
 // quando o evento 'scroll' ocorrer.
 
+addEventListener('scroll', function(){
+    console.log('Júlio Monteiro');
+});
+
 // Corrija o erro abaixo
-// function precisoVisitar(paisesVisitados) {
-//   var totalPaises = 193;
-//   return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
-// }
-// function jaVisitei(paisesVisitados) {
-//   return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
-// }
-// precisoVisitar(20);
-// jaVisitei(20);
+var totalPaises = 193;
+
+ function precisoVisitar(paisesVisitados) {
+
+   return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+}
+
+
+ function jaVisitei(paisesVisitados) {
+   return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+ }
+console.log(precisoVisitar(20));
+console.log(jaVisitei(20));
--- funcoes1/script.js
+++ funcoes1/script.js
@@ -11,3 +11,68 @@ function pi() {
 var total = 5 * pi();
 
 console.log(total);
+
+function imc(peso, altura) {
+    var imc = peso /(altura * altura);
+    return imc
+}
+
+console.log(imc(80, 1.8))
+
+function corFavorita(cor) {
+    if (cor === 'azul') {
+        return 'Você gosta do céu'
+    } else if (cor === 'verde') {
+        return 'Você gosta de mato'
+    } else {
+        return 'Você não gosta de nada'
+    }
+}
+console.log(corFavorita());
+
+
+addEventListener ('click', function() {
+    console.log('Clicou'); 
+});
+
+function imc2 (peso, altura) {
+    const imc = peso / (altura ** 2)
+    console.log(imc);
+}
+
+imc2(20, 1.8);
+
+function terceiraIdade(idade) {
+    if(typeof idade !== 'number') {
+        return 'Por favor preencha um número'
+    } else if(idade >= 60) {
+        return true;
+    } else {
+        return false;
+    }
+    
+}
+
+console.log(terceiraIdade('oi'))
+
+function faltaVisitar(paisesVisitados) {
+    var totalPaises = 193;
+
+    return `Falta visitar ${totalPaises - paisesVisitados} países`
+
+}
+
+var profissao = 'Designer';
+
+function dados() {
+    var nome = 'Júlio';
+    var idade = '18';
+    function outrosDados() {
+        var endereco = 'Sergipe';
+        var idade = 19;
+        return `${nome}, ${idade}, ${endereco}, ${profissao}`;
+    }
+    return outrosDados()
+}
+
+console.log(dados())
