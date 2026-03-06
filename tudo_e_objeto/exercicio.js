104f2295fc4d7b49865f2169842502b461d6c049
Júlio Monteiro <julioalvedmonteiro@gmail.com>
2026-03-06 20:54:41 -0300

    Aprendendo sobre object

---
+++ tudo_e_objeto/exercicio.js
@@ -0,0 +1,20 @@
+// nomeie 3 propriedades ou métodos de strings
+
+var nome = 'Júlio';
+
+nome.length;
+nome.charAt(3);
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector('.btn');
+//addEventListener
+//appendChild
+//call
+//innerHtml
+//id
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+
+ readText()
+
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
---
+++ tudo_e_objeto/script.js
@@ -0,0 +1,27 @@
+var nome = 'Júlio';
+
+nome.length; //5
+nome.charAt(1); //ú
+nome.replace('o', 'ano'); //Júliano
+nome; // 'Júlio"
+
+//Números
+
+var altura = 1.76;
+
+altura.toString(); // '1,8'
+altura.toFixed(); // '2'
+
+//funções
+
+function areaQuadrado(lado) {
+    return lado * lado;
+}
+
+areaQuadrado.toString();
+
+ //1
+
+//elementos do Dom
+
+var btn = document.querySelector('.btn');
