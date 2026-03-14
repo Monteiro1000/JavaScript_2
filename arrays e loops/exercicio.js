688c4eaeed7efdc6b9e9f9109c5f24169b9ab1b6
Júlio Monteiro <julioalvedmonteiro@gmail.com>
2026-03-14 14:53:59 -0300

    finalizando exercício sobre arrays e loops

--- arrays e loops/exercicio.js
+++ arrays e loops/exercicio.js
@@ -1,29 +1,37 @@
-// Crie uma array com os anos que o Brasil ganhou a copa
-// 1959, 1962, 1970, 1994, 2002
+// // Crie uma array com os anos que o Brasil ganhou a copa
+// // 1959, 1962, 1970, 1994, 2002
 
-var brGanhouCopa = [1959, 1962, 1970, 1994, 2002];
+// var brGanhouCopa = [1959, 1962, 1970, 1994, 2002];
 
 
-// Interaja com a array utilizando um loop, para mostrar
-// no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
-for(var i = 0; i < brGanhouCopa.length; i++ ) {
-    console.log(`O Brasil ganhou a copa de ${brGanhouCopa[i]}`);
-}
+// // Interaja com a array utilizando um loop, para mostrar
+// // no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
+// for(var i = 0; i < brGanhouCopa.length; i++ ) {
+//     console.log(`O Brasil ganhou a copa de ${brGanhouCopa[i]}`);
+// }
 
-// Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
+// // Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
 var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância']
 
-var c = 0;
+for(var i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+
+    if(frutas[i] === 'Pera') {
+        break;
+    }
+}
+
+// var c = 0;
 // while (c < 3) {
 //     console.log(frutas[c]);
 //     c++;
 // }
 
-if (4 <= frutas.length) {
-    c = frutas[4];
-}
+// if (4 <= frutas.length) {
+//     c = frutas[4];
+// }
 
-console.log(c);
+// console.log(c);
 
 // Coloque a última fruta da array acima em uma variável,
 // sem remover a mesma da array.
--- arrays e loops/script.js
+++ arrays e loops/script.js
@@ -1,6 +1,41 @@
+
+
+// console.log(videoGames.pop()); 
+// videoGames.push('3DS');
+
+// var ultimoItem = videoGames.pop();
+
+// //loop
+
+// for (var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// //while
+
+// var numero = 0;
+
+// while (numero < 10) {
+//     console.log(numero);
+//     numero++;
+// }
+
+//arrays e loops
+
 var videoGames = ['Switch', 'PS4', 'XBox'];
 
-console.log(videoGames.pop()); 
-videoGames.push('3DS');
+// for(var item = 0; item < videoGames.length; item++) {
+//     console.log(videoGames[item]);
+//     //break
+//     if(videoGames[item] === 'PS4')
+//     break;
+// }
 
-var ultimoItem = videoGames.pop();
+//forEach
+
+videoGames.forEach(function(item, index, array) {
+    console.log(item, index, array);
+}
+
+)
+
